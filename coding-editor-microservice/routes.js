c24b4ea2539df22be520a2829b1a952e7adabc58
Sefatullah Omar <sefaullahomar1@gmail.com>
2022-12-18 07:34:22 -0800

    make it better

--- coding-editor-microservice/routes.js
+++ coding-editor-microservice/routes.js
@@ -2,8 +2,6 @@ const router = require('express').Router();
 const roomsController = require('./controllers/roomsController');
 
 
-
-
 router.put("/api/XMLCodes", roomsController.XMLcodes)
 router.put("/api/CSSCodes", roomsController.CSScodes)
 router.put("/api/JSCodes", roomsController.JScodes)
